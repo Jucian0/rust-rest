5e9a28d6d968998ce6078b24ea333ebd34b6e7a3
jucian0 <juciano@outlook.com.br>
2021-05-29 23:38:34 -0300

    add api configurations

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_migrations;
 
+use actix_web::Responder;
+use actix_web::HttpRequest;
 use actix_redis::RedisSession;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web, HttpResponse};
+use serde::{Deserialize, Serialize};
 use dotenv::dotenv;
 use std::env;
 
@@ -17,6 +20,17 @@ mod email;
 mod email_verification_token;
 mod schema;
 mod user;
+
+#[derive(Serialize, Deserialize)]
+pub struct ApiDescription {
+    pub apiName: String,
+    pub version: String
+}
+
+async fn index(req: HttpRequest)->impl Responder{
+    HttpResponse::Ok().json(ApiDescription{apiName:"Api Rust Rest".to_string(), version:"1.0".to_string()})
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -36,6 +50,7 @@ async fn main() -> std::io::Result<()> {
                 format!("{}:{}", redis_host, redis_port),
                 &[0; 32],
             ))
+            .route("/", web::get().to(index))
             .configure(user::init_routes)
             .configure(auth::init_routes)
     })
